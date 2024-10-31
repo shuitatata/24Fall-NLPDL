f71e11cb799e11e9a6b92cdecfaff7394ea4a01d
shuitatata <2462703357@qq.com>
2024-10-31 21:29:36 +0800

    finish hw2 task1

--- hw2/dataHelper.py
+++ hw2/dataHelper.py
@@ -1,5 +1,7 @@
 from datasets import load_dataset, Dataset, DatasetDict
 import json
+import jsonlines
+import pandas as pd
 
 def load_SemEval14(path, sep_token):
 
@@ -7,15 +9,32 @@ def load_SemEval14(path, sep_token):
 
     with open (path + "/train.json", 'r') as f:
         data = json.load(f)
-    train_data = [{"text": data[id]["term"]+sep_token+data[id]["sentence"], "label": polarity_map[data[id]["polarity"]]} for id in data]
+    train_data = [{"text": data[id]["term"]+sep_token+data[id]["sentence"], "labels": polarity_map[data[id]["polarity"]]} for id in data]
     
     with open (path + "/test.json", 'r') as f:
         data = json.load(f)
-    test_data = [{"text": data[id]["term"]+sep_token+data[id]["sentence"], "label": polarity_map[data[id]["polarity"]]} for id in data]
+    test_data = [{"text": data[id]["term"]+sep_token+data[id]["sentence"], "labels": polarity_map[data[id]["polarity"]]} for id in data]
     train_dataset = Dataset.from_list(train_data)
     test_dataset = Dataset.from_list(test_data)
     return train_dataset, test_dataset
         
+def load_aclarc(path, sep_token):
+    data = {}
+    train_data = []
+    test_data= []
+    label_map = {'Uses': 0, 'Future': 1, 'CompareOrContrast': 2,
+                     'Motivation': 3, 'Extends': 4, 'Background': 5}
+    with open(path+"/train.jsonl", 'r') as f:
+        raw_data = jsonlines.Reader(f)
+        for item in raw_data:
+            train_data += [{"text": item["text"], "labels": label_map[item["label"]]}]
+
+    with open(path+"/test.jsonl", 'r') as f:
+        raw_data = jsonlines.Reader(f)
+        for item in raw_data:
+            test_data += [{"text": item["text"], "labels": label_map[item["label"]]}]
+
+    return DatasetDict({"train": Dataset.from_list(train_data), "test": Dataset.from_list(test_data)})
 
 def get_dataset(dataset_name, sep_token):
     '''
@@ -34,11 +53,51 @@ def get_dataset(dataset_name, sep_token):
         dataset = DatasetDict({"train": train_dataset, "test": test_dataset})
         return dataset
 
-    print(dataset['train'][0])
+    if dataset_name == "acl_sup":
+        dataset = load_aclarc("data/acl_sup", sep_token)
+        return dataset
+
+    if dataset_name == "agnews_sup":
+        # dataset = load_dataset("csv", data_files="data/agnews_sup/agnews_sup.csv")
+        df = pd.read_csv('data/agnews_sup/agnews_sup.csv', header=0)
+        df['labels'] = df['labels'] - 1
+        dataset = Dataset.from_pandas(df)
+        # print(dataset)
+        dataset = dataset.train_test_split(test_size=0.1, seed=2022, shuffle=True)
+        return dataset
+
+    if "fs" in dataset_name:
+        dataset = get_dataset(dataset_name[:-2]+"sup", sep_token)
+        dataset["train"] = dataset["train"].select(range(32))
+        # dataset = DatasetDict({"train": dataset["train"]
+        # [:32], "test": dataset["test"]})
+        return dataset
+
+    # dataset_name是列表
+    if isinstance(dataset_name, list):
+        all_train = []
+        all_test = []
+        all_label = set()
+        for name in dataset_name:
+            dataset = get_dataset(name, sep_token)
+            label_offset = len(all_label) 
+            dataset['train'] = dataset['train'].map(lambda x: {'text': x['text'], 'labels': x['labels']+label_offset})
+            dataset['test'] = dataset['test'].map(lambda x: {'text': x['text'], 'labels': x['labels']+label_offset})
+            all_train += dataset["train"]
+            all_test += dataset["test"]
+            all_label = all_label.union(set(dataset["train"]["labels"]))
+        dataset = DatasetDict({"train": Dataset.from_list(all_train), "test": Dataset.from_list(all_test)})
+        print(all_label)
+
+
     # print(type(dataset))
 
     return dataset
 
 if __name__ == '__main__':
-    dataset = get_dataset("laptop_sup", "[SEP]")
-    print(dataset["train"][0])
+    # dataset = get_dataset("restaurant_sup", "[SEP]")
+    # print(dataset['train'][0])
+    # print(load_aclarc("data/acl_sup", "[SEP]"))
+    print(get_dataset("agnews_sup", "[SEP]"))
+    print(get_dataset("acl_sup", "[SEP]"))
+    print(get_dataset(["agnews_sup", "acl_sup"], "[SEP]"))
